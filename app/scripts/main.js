20bc5e8cf9ba94832c6be16a8c8b11ee6765f0e0
Emory Griffith <emorygriffith91@gmail.com>
2016-04-30 18:26:36 -0400

    got colors array working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,26 +1,30 @@
-var red = data.colors.red;
+// var red = data.colors.red;
+//
+//
+// $( ".submit" ).click(function() {
+//
+//   //var value = $("form input[type='checkbox']:checked").val();
+//
+//   if ($(".fresh").is(':checked')) {
+//     console.log("fresh is checked");
+//
+//     red++;
+//     console.log(red);
+//
+//   };
+//
+//
+//   if ($(".friendly").is(':checked')) {
+//     console.log("friendly is checked");
+//
+//     red++;
+//
+//     console.log(red);
+//
+//   };
+//
+// });
 
-
-$( ".submit" ).click(function() {
-
-  //var value = $("form input[type='checkbox']:checked").val();
-
-  if ($(".fresh").is(':checked')) {
-    console.log("fresh is checked");
-
-    red++;
-    console.log(red);
-
-  };
-
-
-  if ($(".friendly").is(':checked')) {
-    console.log("friendly is checked");
-
-    red++;
-
-    console.log(red);
-
-  };
-
+$.each(colors, function(key, value) {
+    console.log(key + ' ' + value.description);
 });
--- app/scripts/questions.js
+++ app/scripts/questions.js
@@ -1,17 +1,89 @@
-var data =
-{
+var colors = [
 
+      {
+        "id": 0,
+        "name": "Yellow, Dark-Grey, Light-Grey",
+        "count": 0,
+        "description": "this is a placeholder"
+      },
+      {
+        "id": 1,
+        "name": "Red, Dark-Blue, White",
+        "count": 0,
+        "description": "this is a placeholder"
+      },
+      {
+        "id": 2,
+        "name": "Bright-Blue, Aqua-Green, Dark-Grey",
+        "count": 0,
+        "description": "this is a placeholder"
+      },
+      {
+        "id": 3,
+        "name": "Green-Grey, Red-Orange, White",
+        "count": 0,
+        "description": "this is a placeholder"
+      }
 
-  "colors": {
-    "red": 0,
-    "blue": 0,
-    "green": 0
-  },
-  "fonts": {
-    "open-sans": 0,
-    "lato": 0,
-    "comic-sans": 0
-  }
+];
+//
+// var data =
+// {
+//
+//
+//   "colors": {
+//
+//     yellow-dark-grey-light-grey: {
+//         "count": 0,
+//         "description": "blah blah blah actual names here"
+//       },
+//     red-dark-blue-white: {
+//         "count": 0,
+//         "description": "blah blah blah actual names here"
+//       },
+//     bright-blue-grey-light-grey: {
+//         "count": 0,
+//         "description": "blah blah blah actual names here"
+//       },
+//     bright-blue-aqua-green-dark-grey: {
+//         "count": 0,
+//         "description": "blah blah blah actual names here"
+//       },
+//     green-grey-red-orange-dark-grey: {
+//         "count": 0,
+//         "description": "blah blah blah actual names here"
+//       },
+//
+//
+//     "Green-Grey, Red-Orange, White": 0
+//   },
+  // "wireframes": {
+  //   "startup": 0,
+  //   "small business": 0,
+  //   "blog": 0,
+  //   "e-commerce": 0,
+  //   "restaurant": 0
+  // },
+  // "fonts": {
+  //   "sophisticated": 0,
+  //   "clean": 0,
+  //   "organized": 0,
+  //   "esoteric": 0,
+  //   "luxury": 0,
+  //   "craft": 0
+  // },
+  // "tone of copy": {
+  //   "formal": 0,
+  //   "business": 0,
+  //   "conversational": 0,
+  //   "attitude": 0,
+  //   "edgy": 0
+  // },
+  // "logo": {
+  //   "text": 0,
+  //   "icon": 0,
+  //   "logotype": 0,
+  //   "calligraphy": 0
+  // }
 
-
-};
+//};
