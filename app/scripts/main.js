45521feed05faeed8cc0d3d673d2e6d38843bd72
Emory Griffith <emorygriffith91@gmail.com>
2016-04-30 12:32:18 -0400

    added backbone, jquery, bootstrap, underscore, and a questions template

---
+++ app/scripts/main.js
@@ -0,0 +1,46 @@
+(function() {
+
+//---------------VIEWS--------------//
+var QuestionsView = Backbone.View.extend({
+  el: '.app',
+  render: function(){
+    var template = _.template($('#questions-template').html());
+    this.$el.html(template);
+  },
+  error: function(){
+    alert('error rendering view');
+  }
+});
+var questionsView = new QuestionsView();
+
+
+
+
+
+
+
+//---------------ROUTES--------------//
+var Router = Backbone.Router.extend({
+  routes: {
+    '': 'questions',
+
+  }
+});
+
+var router = new Router();
+
+router.on('route:questions', function(){
+  questionsView.render();
+});
+
+
+
+
+
+
+
+
+//-------Start History------//
+Backbone.history.start();
+
+})();
