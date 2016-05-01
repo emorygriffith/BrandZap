b177c512364ea3e14ae22fa419eeeabcfb2bbfb7
Emory Griffith <emorygriffith91@gmail.com>
2016-05-01 18:13:16 -0400

    added final questions and logic... complete logic for now. need styles.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,7 +7,99 @@ Backbone.history.start();
 
 $( ".submit" ).click(function() {
 
-//QUESTION 1
+
+  //QUESTION 1
+  if ($(".kids").is(':checked')) {
+    colors[2].count++;
+
+    wireframes[0].count++;
+    wireframes[2].count++;
+
+    fonts[1].count++;
+    fonts[5].count++;
+
+    copy[2].count++;
+    copy[3].count++;
+
+    logo[0].count++;
+  };
+  if ($(".teens").is(':checked')) {
+    colors[0].count++;
+    colors[2].count++;
+
+    wireframes[0].count++;
+    wireframes[2].count++;
+
+    fonts[1].count++;
+    fonts[3].count++;
+
+    copy[2].count++;
+    copy[3].count++;
+    copy[4].count++;
+
+    logo[0].count++;
+    logo[2].count++;
+  };
+  if ($(".millenials").is(':checked')) {
+    colors[0].count++;
+    colors[2].count++;
+
+    wireframes[0].count++;
+    wireframes[1].count++;
+    wireframes[2].count++;
+
+    fonts[0].count++;
+    fonts[1].count++;
+    fonts[3].count++;
+    fonts[5].count++;
+
+    copy[2].count++;
+    copy[3].count++;
+
+    logo[0].count++;
+    logo[1].count++;
+    logo[2].count++;
+    logo[3].count++;
+  };
+  if ($(".parents").is(':checked')) {
+    colors[0].count++;
+    colors[1].count++;
+
+    wireframes[1].count++;
+    wireframes[2].count++;
+    wireframes[3].count++;
+    wireframes[4].count++;
+
+    fonts[0].count++;
+    fonts[1].count++;
+    fonts[2].count++;
+    fonts[4].count++;
+
+    copy[0].count++;
+    copy[1].count++;
+    copy[2].count++;
+
+    logo[0].count++;
+    logo[2].count++;
+  };
+  if ($(".grandparents").is(':checked')) {
+    colors[3].count++;
+
+    wireframes[4].count++;
+
+    fonts[0].count++;
+    fonts[1].count++;
+    fonts[4].count++;
+
+    copy[0].count++;
+    copy[1].count++;
+
+    logo[0].count++;
+    logo[2].count++;
+    logo[3].count++;
+  };
+
+//QUESTION 2
   if ($(".fresh").is(':checked')) {
     colors[2].count++;
     colors[3].count++;
@@ -224,6 +316,239 @@ $( ".submit" ).click(function() {
     copy[3].count++;
 
   };
+
+//QUESTION 3
+if ($(".cleanAndSimple").is(':checked')) {
+  colors[0].count++;
+  colors[1].count++;
+
+  wireframes[0].count++;
+
+  fonts[1].count++;
+  fonts[4].count++;
+
+  copy[2].count++;
+
+  logo[1].count++;
+};
+
+if ($(".wellOrgWithLotsOfInfo").is(':checked')) {
+  colors[0].count++;
+  colors[1].count++;
+
+  wireframes[0].count++;
+  wireframes[3].count++;
+
+  fonts[1].count++;
+  fonts[2].count++;
+
+  logo[0].count++;
+  logo[1].count++;
+};
+
+if ($(".sophisticated").is(':checked')) {
+  colors[0].count++;
+  colors[2].count++;
+
+  wireframes[4].count++;
+
+  fonts[0].count++;
+  fonts[4].count++;
+
+  copy[0].count++;
+
+  logo[1].count++;
+};
+
+if ($(".powerful").is(':checked')) {
+  colors[0].count++;
+
+  fonts[4].count++;
+
+  copy[3].count++;
+  copy[4].count++;
+
+  logo[1].count++;
+};
+
+if ($(".boldAndElectric").is(':checked')) {
+  colors[0].count++;
+  colors[2].count++;
+
+  wireframes[0].count++;
+
+  fonts[4].count++;
+
+  copy[3].count++;
+  copy[4].count++;
+
+  logo[2].count++;
+};
+
+
+
+//QUESTION 4
+if ($(".luxury").is(':checked')) {
+  colors[0].count++;
+  colors[2].count++;
+
+  wireframes[4].count++;
+
+  fonts[0].count++;
+  fonts[4].count++;
+
+  copy[0].count++;
+
+  logo[1].count++;
+  logo[3].count++;
+};
+
+if ($(".peerToPeer").is(':checked')) {
+
+  fonts[1].count++;
+
+  copy[2].count++;
+
+  logo[2].count++;
+};
+
+if ($(".neighborhood").is(':checked')) {
+  colors[3].count++;
+
+  wireframes[4].count++;
+
+  fonts[5].count++;
+  fonts[0].count++;
+
+  copy[2].count++;
+
+  logo[0].count++;
+  logo[2].count++;
+  logo[3].count++;
+
+};
+
+if ($(".informationalBlogSite").is(':checked')) {
+  colors[0].count++;
+
+  fonts[1].count++;
+  fonts[2].count++;
+
+  copy[1].count++;
+  copy[2].count++;
+
+  logo[0].count++;
+};
+
+if ($(".creativeService").is(':checked')) {
+  colors[0].count++;
+
+  wireframes[0].count++;
+
+  fonts[0].count++;
+  fonts[3].count++;
+  fonts[4].count++;
+
+  copy[1].count++;
+  copy[2].count++;
+
+  logo[0].count++;
+  logo[2].count++;
+};
+
+
+
+//QUESTION 5
+if ($(".application").is(':checked')) {
+  colors[2].count++;
+
+  wireframes[0].count++;
+
+  fonts[1].count++;
+  fonts[2].count++;
+
+  copy[1].count++;
+  copy[2].count++;
+
+  logo[1].count++;
+  logo[2].count++;
+
+};
+
+if ($(".restaurant").is(':checked')) {
+  colors[1].count++;
+  colors[3].count++;
+
+  wireframes[4].count++;
+
+  fonts[0].count++;
+  fonts[4].count++;
+  fonts[5].count++;
+
+  copy[1].count++;
+  copy[2].count++;
+
+  logo[0].count++;
+  logo[3].count++;
+};
+if ($(".website").is(':checked')) {
+  colors[0].count++;
+  colors[1].count++;
+
+  wireframes[1].count++;
+  wireframes[3].count++;
+
+  fonts[1].count++;
+  fonts[2].count++;
+
+  copy[1].count++;
+  copy[2].count++;
+
+  logo[0].count++;
+  logo[2].count++;
+};
+if ($(".eCommerce").is(':checked')) {
+  colors[1].count++;
+
+  wireframes[3].count++;
+
+  fonts[1].count++;
+  fonts[2].count++;
+
+  copy[1].count++;
+
+  logo[0].count++;
+  logo[1].count++;
+  logo[2].count++;
+
+};
+if ($(".brickAndMortar").is(':checked')) {
+  colors[2].count++;
+  colors[3].count++;
+
+  wireframes[1].count++;
+
+  fonts[2].count++;
+  fonts[5].count++;
+
+  copy[2].count++;
+  copy[1].count++;
+
+  logo[0].count++;
+};
+if ($(".professionalServices").is(':checked')) {
+  colors[1].count++;
+
+  wireframes[1].count++;
+  wireframes[3].count++;
+
+  fonts[2].count++;
+
+  copy[1].count++;
+
+  logo[2].count++;
+  logo[0].count++;
+};
 
 
 
